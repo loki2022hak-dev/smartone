11d229e731b141b4fe922b340fcb0b3a10434611
loki2022hak-dev <loki2022hak-dev@users.noreply.github.com>
2026-04-14 02:47:41 +0300

    fix: cleanup catalog logic and ensure lib/utils exports

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,13 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export const formatPrice = (price: number | string) => {
-  return new Intl.NumberFormat('uk-UA', { style: 'currency', currency: 'UAH' }).format(Number(price));
+  return new Intl.NumberFormat('uk-UA', {
+    style: 'currency',
+    currency: 'UAH',
+  }).format(Number(price));
 };
